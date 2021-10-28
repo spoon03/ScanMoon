ed93479cf7a3e8df395625a40fa6ea49470fef1f
FyodorovMS <spoon03@mail.ru>
2021-10-28 22:27:26 +0300

    ready 1.0

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+"""Поиск кратеров на луне."""
+
+
 def open_file(name: str) -> list:
+    """
+    Читаем из файла карту луны.
+
+    :param name:Имя файла.
+    :return: Матрица.
+    """
     text = []
     row = []
-    i = 0
     with open(name, 'r') as f:
         for ch in f.read():
             if ch != '\n':
@@ -13,31 +21,23 @@ def open_file(name: str) -> list:
     return text
 
 
-def calculate(photo: str):
-    data = open_file(photo)
-    line_max = len(data)
-    colum_max = len(data[0])
-    count = 0
-    for d in data:
-        print(d)
-    print('===============')
-    for x in range(line_max):
-        for y in range(colum_max):
-            el = data[x][y]
-            print(f'Элемент с координатой {x + 1}{y + 1}={el}')
-            if el == '1':
-                data = scan(x, y, data, line_max, colum_max)
-                count += 1
-    return count
+def scan(x: int, y: int, data: list, x_max: int, y_max: int) -> list:
+    """
+    Сканирование кратера.
 
-def scan(x: int, y: int, data: list, x_max, y_max):
+    :param x: Координата части кратера
+    :param y: Координата части кратера
+    :param data: Снимок луны
+    :param x_max: Макс размер x
+    :param y_max: Макс размер y
+    :return: Снимок с отмеченным кратером
+    """
     data[x][y] = 'x'
     if x + 1 < x_max:
         el = data[x + 1][y]
         if el == '1':
             data[x + 1][y] = 'x'
             scan(x + 1, y, data, x_max, y_max)
-
     if y + 1 < y_max:
         el = data[x][y + 1]
         if el == '1':
@@ -53,10 +53,27 @@ def scan(x: int, y: int, data: list, x_max, y_max):
         if el == '1':
             data[x][y - 1] = 'x'
             scan(x, y - 1, data, x_max, y_max)
-    for d in data:
-        print(d)
-    print('===============')
     return data
+
+
+def calculate(photo: str) -> int:
+    """
+    Подсчет кол-ва кратеров.
+
+    :param photo: Имя файла с фото
+    :return: Кол-во кратеров
+    """
+    data = open_file(photo)
+    line_max = len(data)
+    colum_max = len(data[0])
+    count = 0
+    for x in range(line_max):
+        for y in range(colum_max):
+            el = data[x][y]
+            if el == '1':
+                data = scan(x, y, data, line_max, colum_max)
+                count += 1
+    return count
 
 
 print(calculate('photo.txt'))
